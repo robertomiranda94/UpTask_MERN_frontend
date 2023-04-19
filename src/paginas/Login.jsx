50e147638f3249225debf647e1628254ad1f4b4c
Roberto <mirandaroberto1994@gmail.com>
2023-04-19 02:47:38 -0300

    se envia token por email para reestablecer contraseña, se implementa axios y se autentica usuarios

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,13 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [alerta, setAlerta] = useState({});
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if ([email, password].includes("")) {
+            setAlerta({
+                mensaje: "Todos los campos son obligatorios",
+                error: true,
+            });
+            return;
+        }
+
+        try {
+            const { data } = await clienteAxios.post("/usuarios/login", {
+                email,
+                password,
+            });
+            setAlerta({});
+            localStorage.setItem("token", data.token);
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true,
+            });
+        }
+    };
+    const { msg } = alerta;
     return (
         <>
             <h1 className="text-sky-600 font-black text-6xl capitalize">
                 Inicia sesión y administra tus{" "}
                 <span className="text-slate-700">proyectos</span>
             </h1>
-            <form className="my-10 bg-white shadow rounded-lg p-10">
+            {msg && <Alerta alerta={alerta} />}
+            <form
+                className="my-10 bg-white shadow rounded-lg p-10"
+                onSubmit={handleSubmit}
+            >
                 <div className="my-5">
                     <label
                         className="uppercase text-gray-600 block text-xl font-bold"
@@ -20,6 +57,8 @@ const Login = () => {
                         type="text"
                         placeholder="Email de Registro"
                         className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
 
@@ -35,6 +74,8 @@ const Login = () => {
                         type="password"
                         placeholder="Password de Registro"
                         className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
 
--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,13 +1,53 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
 
 const OlvidePassword = () => {
+    const [email, setEmail] = useState("");
+    const [alerta, setAlerta] = useState({});
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (email === "" || email.length < 6) {
+            setAlerta({
+                msg: "El Email es obligatorio",
+                error: true,
+            });
+            return;
+        }
+
+        try {
+            const { data } = await clienteAxios.post(
+                `/usuarios/olvide-password`,
+                { email }
+            );
+
+            setAlerta({
+                msg: data.msg,
+                error: false,
+            });
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true,
+            });
+        }
+    };
+    const { msg } = alerta;
     return (
         <>
             <h1 className="text-sky-600 font-black text-6xl capitalize">
                 Recupera tu acceso y no pierdas tus{" "}
                 <span className="text-slate-700">proyectos</span>
             </h1>
-            <form className="my-10 bg-white shadow rounded-lg p-10">
+
+            {msg && <Alerta alerta={alerta} />}
+            <form
+                className="my-10 bg-white shadow rounded-lg p-10"
+                onSubmit={handleSubmit}
+            >
                 <div className="my-5">
                     <label
                         className="uppercase text-gray-600 block text-xl font-bold"
@@ -20,6 +60,8 @@ const OlvidePassword = () => {
                         type="text"
                         placeholder="Email de Registro"
                         className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
 
