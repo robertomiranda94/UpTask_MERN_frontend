d2cdd06724a6def5facec0f42be3463a84bfa48b
Roberto <mirandaroberto1994@gmail.com>
2023-04-20 02:24:26 -0300

    crea area privada - components y context - Formulario creacion de  Proyectos

---
+++ src/paginas/Proyectos.jsx
@@ -0,0 +1,15 @@
+import useProyectos from "../hooks/useProyectos";
+
+
+const Proyectos = () => {
+
+    const { proyectos } = useProyectos();
+    return (
+        <>
+            <h1 className="text-4xl font-black">Proyectos</h1>
+            <div></div>
+        </>
+    );
+};
+
+export default Proyectos;
